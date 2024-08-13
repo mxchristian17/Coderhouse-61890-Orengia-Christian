d577f74ee53f91440b3aa692199385a7e88dc34e
Christian Orengia <christian.o@growketing.com>
2024-08-12 22:51:43 -0300

    Started 2nd delivery. Transformed population data, remains to end the transform of the wheather data working on it

---
+++ 2nd-delivery/data/db-setup/setup.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS population_data;
+CREATE TABLE population_data (
+    id_event INT IDENTITY(1,1) PRIMARY KEY,
+    females INT,
+    country VARCHAR(250),
+    age SMALLINT,
+    males INT,
+    year SMALLINT,
+    total INT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS weather_data;
+CREATE TABLE weather_data (
+    id_event INT IDENTITY(1,1) PRIMARY KEY,
+    event_data VARCHAR(MAX)
+);
+
+SELECT 
+  column_name, 
+  data_type, 
+  character_maximum_length, 
+  is_nullable, 
+  column_default 
+FROM 
+  information_schema.columns 
+WHERE 
+  table_name = 'population_data';
+
+SELECT 
+  column_name, 
+  data_type, 
+  character_maximum_length, 
+  is_nullable, 
+  column_default 
+FROM 
+  information_schema.columns 
+WHERE 
+  table_name = 'weather_data';
+
+DROP TABLE IF EXISTS population_weather_relation;
+CREATE TABLE population_weather_relation (
+    id_event INT IDENTITY(1,1) PRIMARY KEY,
+    population_change FLOAT,
+    average_temperature_change FLOAT,
+    location VARCHAR(100),
+    year INT
+);
